7a3a5355cbc658082686897243b872f8a26c0ae2
Oliver Widder <oliver.widder@gmail.com>
2015-06-14 19:29:00 +0000

    gitignore + config

---
+++ src/nodejs/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  app_id: "124797",
+  key: "8b6aa40fbb5700a4aa2e",
+  secret: "5931e049f8e5c8305d7a"
+}
---
+++ src/ruby-sinatra/config.rb
@@ -0,0 +1,5 @@
+require 'pusher'
+
+Pusher.app_id = '124797'
+Pusher.key = '8b6aa40fbb5700a4aa2e'
+Pusher.secret = '5931e049f8e5c8305d7a'
